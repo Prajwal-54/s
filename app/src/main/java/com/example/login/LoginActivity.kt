193fc668521389c94174a4ea0cf54cf92be16fe1
prajwal-54 <prajjumoger54@gmail.com>
2022-07-13 01:23:38 +0530

    commit
    signup&signIn

---
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.example.login
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import androidx.core.view.isInvisible
+import androidx.room.Room
+import kotlinx.android.synthetic.main.login.*
+import java.lang.Exception
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        supportActionBar?.setTitle("Log In")
+
+        val db = Room.databaseBuilder(applicationContext, AppDB::class.java, "app-database").allowMainThreadQueries().build()
+        val userDao = db.userDao()
+
+        var count = 0
+
+        logInButton.setOnClickListener {
+            try{
+                val tempUser = userDao.getUser(login_username.text.toString())
+                if(tempUser.password.equals(login_password.text.toString())){
+                    Toast.makeText(baseContext,"Login successful",Toast.LENGTH_SHORT).show()
+                    count = 0
+                }
+                else{
+                    if(count<3){
+                        Toast.makeText(baseContext,"Invalid Username Or Password",Toast.LENGTH_SHORT).show()
+                        count++
+                    }
+                    else{
+                        Toast.makeText(baseContext,"Failed, Log in disabled",Toast.LENGTH_SHORT).show()
+                        logInButton.isEnabled = false
+                    }
+                }
+            }
+            catch (e: Exception) {
+                Toast.makeText(baseContext, "Invalid Username", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        login_to_signup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/login/UserDAO.kt
@@ -0,0 +1,14 @@
+package com.example.login
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao    //Data Access object
+interface UserDao {
+    @Query("SELECT * FROM user WHERE username == :username")
+    fun getUser(username: String): User
+    @Insert
+    fun insert(user: User)
+}
